3d9531ef446a749c39081cb7267d4b9d8be1005a
creartix <marcel@creartix.de>
2026-03-11 18:31:07 +0100

    Updated Theme

--- content/themes/fitvibes/vite.config.js
+++ content/themes/fitvibes/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
         outDir: 'assets/built',
         emptyOutDir: true,
         manifest: true,
+        watch: {
+            include: ['assets/**', '**/*.hbs']
+        },
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'assets/css/main.css'),
@@ -21,6 +24,12 @@ export default defineConfig({
     server: {
         port: 3000,
         strictPort: true,
+        host: 'localhost',
         cors: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+        }
     }
 });
