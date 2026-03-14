3f282348ac18d8d116e0d8ca45af22a553ea72c1
creartix <marcel@creartix.de>
2026-03-14 14:57:05 +0100

    Updated Theme

--- content/themes/fitvibes/vite.config.js
+++ content/themes/fitvibes/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/assets/built/',
     build: {
         outDir: 'assets/built',
         emptyOutDir: true,
